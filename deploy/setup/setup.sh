99409d6fd3bad37bb8cb0b23241034def14b8c79
iwnl-NWK <102374247+iwnl-NWK@users.noreply.github.com>
2024-04-30 20:37:09 -0300

    Update setup.sh

--- deploy/setup/setup.sh
+++ deploy/setup/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "\n\nFazendo a configuracao no /opt, e instalando o NGINX [...]\n"
-sudo mkdir /opt/challenge && sudo mv ./setup/* /opt/challenge && sudo cd /opt/challenge;
-sudo tar xfz ./nginx-1.25.4.tar.gz;
-cd nginx-1.25.4 && sudo ./configure && sudo make && sudo make install;
-sudo cat ./nginx.conf > /usr/local/nginx/nginx.conf;
-cd /usr/local/nginx/sbin && sudo ./nginx
+mkdir /opt/challenge && mv ./setup/* /opt/challenge && cd /opt/challenge;
+tar xfz ./nginx-1.25.4.tar.gz;
+cd nginx-1.25.4 && sudo ./configure && make && make install;
+cat ./nginx.conf > /usr/local/nginx/nginx.conf;
+cd /usr/local/nginx/sbin && ./nginx
